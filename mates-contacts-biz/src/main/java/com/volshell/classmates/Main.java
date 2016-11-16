29d9cec64c3ea7eb6e05fbc67d0d00e70ed19ac8
smartvolshell <810014855@qq.com>
2016-11-17 01:38:27 +0800

    git log file -1

--- mates-contacts-biz/src/main/java/com/volshell/classmates/Main.java
+++ mates-contacts-biz/src/main/java/com/volshell/classmates/Main.java
@@ -1,7 +1,17 @@
 package com.volshell.classmates;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Date;
+
 /**
  * Created by volshell on 16-11-16.
  */
 public class Main {
+    private static Logger logger = LoggerFactory.getLogger(Main.class.getName());
+
+    public static void main(String[] args) {
+        logger.error("nice to meet you {},now is {}","volshell",new Date());
+    }
 }
